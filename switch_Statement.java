28e9de4a4d338f87f75745a2a5c2bfabd3fbc3c3
balraj73554 <160026576+balraj73554@users.noreply.github.com>
2024-08-08 14:55:46 +0530

    Switch_Statement /Java
    switch statement is used to apply condition on list of items. when conditions are multiple and difficult to manage in if else block then switch statements or switch block is used

---
+++ switch_Statement.java
@@ -0,0 +1,14 @@
+public class switch_Statement {
+    public static void main(String[] args){
+        String day = "sunday";
+        switch(day){
+            case "sunday":
+            System.out.println("This is sunday");
+            break;
+            case "monday":
+            System.out.println("This is monday");
+            break;
+            default:System.out.println("Not a day");
+        }
+    }
+}
