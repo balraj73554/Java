a44df3f33b4c45777418e2c3be8f30bbe34ede52
balraj73554 <160026576+balraj73554@users.noreply.github.com>
2024-08-08 13:41:28 +0530

    Basic Java Arithmetic Expression /Java
    very basic arithmetic expression in java

---
+++ arithmeticExpressio.java
@@ -0,0 +1,10 @@
+public class arithmeticExpressio {
+    public static void main(String[] args){
+        // Expression = operands and operators
+        // Operands = values,variables,numbers,quantity
+        // Operators = + - * / %
+        int x = 10;
+        x = x * 2;
+        System.out.println(x);
+    }
+}
